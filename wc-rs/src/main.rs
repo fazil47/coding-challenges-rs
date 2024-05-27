3c444a04db9352337318a1a105db9668a53af7c2
Fazil Babu <fazil47babu@gmail.com>
2024-05-27 21:57:09 +0530

    wc tool - 1

--- wc-rs/src/main.rs
+++ wc-rs/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 3 {
+        let flag = args[1].as_str();
+        let filepath = args[2].as_str();
+        let content = std::fs::read_to_string(filepath).expect("Failed to read file");
+
+        if flag == "-c" {
+            println!("{} {}", content.bytes().count(), filepath);
+        }
+    }
 }
