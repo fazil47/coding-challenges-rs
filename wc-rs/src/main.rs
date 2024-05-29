99af0819d4531f76051000ce75080fd6c58f1b14
Fazil Babu <fazil47babu@gmail.com>
2024-05-29 23:39:35 +0530

    Convert to idiomatic Rust

--- wc-rs/src/main.rs
+++ wc-rs/src/main.rs
@@ -3,45 +3,33 @@ use std::io::Read;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    let first_arg = if args.len() >= 2 {
-        args[1].as_str()
-    } else {
-        ""
+    let first_arg = args.get(1).map(|arg| arg.as_str()).unwrap_or("");
+    let filepath = match first_arg {
+        "-c" | "-l" | "-w" | "-m" => args.get(2).map(|arg| arg.as_str()).unwrap_or(""),
+        _ => first_arg,
     };
-    let filepath =
-        if first_arg == "-c" || first_arg == "-l" || first_arg == "-w" || first_arg == "-m" {
-            if args.len() >= 3 {
-                args[2].as_str()
-            } else {
-                ""
-            }
-        } else {
-            first_arg
-        };
-    let content = if filepath == "" {
-        let mut buffer: Vec<u8> = Vec::new();
+
+    let content = if filepath.is_empty() {
+        let mut buffer = Vec::new();
         std::io::stdin().read_to_end(&mut buffer).unwrap();
         String::from_utf8(buffer).unwrap()
     } else {
         std::fs::read_to_string(filepath).unwrap()
     };
+
     let flag = first_arg;
 
-    if flag == "-c" {
-        println!(" {} {}", content.bytes().count(), filepath);
-    } else if flag == "-l" {
-        println!(" {} {}", content.lines().count(), filepath);
-    } else if flag == "-w" {
-        println!(" {} {}", content.split_whitespace().count(), filepath);
-    } else if flag == "-m" {
-        println!(" {} {}", content.chars().count(), filepath);
-    } else {
-        println!(
+    match flag {
+        "-c" => println!(" {} {}", content.bytes().count(), filepath),
+        "-l" => println!(" {} {}", content.lines().count(), filepath),
+        "-w" => println!(" {} {}", content.split_whitespace().count(), filepath),
+        "-m" => println!(" {} {}", content.chars().count(), filepath),
+        _ => println!(
             "\t{}\t{}\t{}\t{}",
             content.lines().count(),
             content.split_whitespace().count(),
             content.bytes().count(),
             filepath
-        );
+        ),
     }
 }
